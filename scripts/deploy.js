46e04a5f5df57622041c82a05b029f3d02692b5a
hemanthnethinti <hemanthnetinti@gmail.com>
2025-12-13 16:45:37 +0530

    add files

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+import hre from "hardhat";
+
+async function main() {
+  const NFT = await hre.ethers.getContractFactory("NFT");
+
+  const nft = await NFT.deploy();
+  await nft.waitForDeployment();
+
+  console.log("NFT deployed to:", await nft.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
